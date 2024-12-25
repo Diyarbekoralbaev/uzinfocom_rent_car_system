d3e78dfa9c435c36376fc0b049efe73a09e6fc14
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-25 16:42:41 +0500

    feat(utls.py): reservations urls included

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RentalViewSet
+from .views import RentalViewSet, ReservationViewSet
 
 router = DefaultRouter()
+router.register(r'reservations', ReservationViewSet)
 router.register(r'', RentalViewSet)
 
 urlpatterns = [
