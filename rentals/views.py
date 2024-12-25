44240ffb7647a84cebda72134056170d3ce5f27f
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-25 10:58:41 +0500

    feat(views.py): is_near_station function implemented to return car to station path"

--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.template.context_processors import request
 from django.views.decorators.gzip import gzip_page
 from drf_yasg import openapi
@@ -13,6 +14,7 @@ from vehicles.models import VehicleModel, VehicleStatusChoices
 from .models import RentalModel, ReservationModel, RentalStatusChoices
 from users.models import UserChoice
 from .serializers import RentalSerializer, ReservationSerializer
+from .utils import is_near_station
 
 
 @method_decorator(gzip_page, name='dispatch')
@@ -35,27 +37,28 @@ class RentalViewSet(viewsets.ModelViewSet):
         return RentalModel.objects.none()
 
     def create(self, request, *args, **kwargs):
-        rentals = RentalModel.objects.filter(client=request.user, status='ACTIVE')
-        if rentals.exists():
-            return Response({"error": "You already have an active rental"}, status=status.HTTP_400_BAD_REQUEST)
-        reservations = ReservationModel.objects.filter(client=request.user, status='CONFIRMED')
-        if reservations.exists():
-            return Response({"error": "You already have an active reservation"}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            user = request.user
-            vehicle = VehicleModel.objects.get(id=request.data['car'])
-            price = vehicle.daily_price
-            start_date = request.data['start_date']
-            end_date = request.data['end_date']
-            total_amount = price * (end_date - start_date).days
-            if user.balance < total_amount:
-                return Response({"error": "Insufficient balance"}, status=status.HTTP_400_BAD_REQUEST)
-            user.balance -= total_amount
-            user.save()
-            request.data['total_amount'] = total_amount
-            request.data['client'] = user.id
-            request.data['status'] = 'PENDING'
-            return super().create(request, *args, **kwargs)
+        with transaction.atomic():
+            rentals = RentalModel.objects.filter(client=request.user, status='ACTIVE')
+            if rentals.exists():
+                return Response({"error": "You already have an active rental"}, status=status.HTTP_400_BAD_REQUEST)
+            reservations = ReservationModel.objects.filter(client=request.user, status='CONFIRMED')
+            if reservations.exists():
+                return Response({"error": "You already have an active reservation"}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                user = request.user
+                vehicle = VehicleModel.objects.get(id=request.data['car'])
+                price = vehicle.daily_price
+                start_date = request.data['start_date']
+                end_date = request.data['end_date']
+                total_amount = price * (end_date - start_date).days
+                if user.balance < total_amount:
+                    return Response({"error": "Insufficient balance"}, status=status.HTTP_400_BAD_REQUEST)
+                user.balance -= total_amount
+                user.save()
+                request.data['total_amount'] = total_amount
+                request.data['client'] = user.id
+                request.data['status'] = 'PENDING'
+                return super().create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
         user = request.user
@@ -104,25 +107,35 @@ class RentalViewSet(viewsets.ModelViewSet):
         return Response({"error": "You do not have permission to set status of a rental"}, status=status.HTTP_403_FORBIDDEN)
 
     @swagger_auto_schema(
-        methods=['get'],
+        methods=['post'],
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
                 'return_station': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'latitude': openapi.Schema(type=openapi.TYPE_NUMBER),
+                'longitude': openapi.Schema(type=openapi.TYPE_NUMBER),
             }
         ),
         responses={200: openapi.Schema(type=openapi.TYPE_OBJECT)}
     )
-    @action(detail=False, methods=['get'], url_path='return-car-to-station', permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['post'], url_path='return-car-to-station', permission_classes=[IsAuthenticated])
     def return_car_to_station(self, request):
         user = request.user
         if user.role == UserChoice.CLIENT:
             rental = RentalModel.objects.get(client=user, status='ACTIVE')
-            if not StationModel.objects.filter(id=request.data['return_station']).exists():
+            station = StationModel.objects.filter(id=request.data['return_station']).first()
+            if not station:
                 return Response({"error": "Station not found"}, status=status.HTTP_400_BAD_REQUEST)
+
+            user_lat = request.data.get('latitude')
+            user_lon = request.data.get('longitude')
+
+            if not is_near_station(user_lat, user_lon, station.latitude, station.longitude):
+                return Response({"error": "You are not near the station"}, status=status.HTTP_400_BAD_REQUEST)
+
             if rental:
                 rental.status = 'COMPLETED'
-                rental.return_station = request.data['return_station']
+                rental.return_station = station
                 rental.save()
                 vehicle = rental.car
                 vehicle.status = VehicleStatusChoices.AVAILABLE
@@ -130,4 +143,5 @@ class RentalViewSet(viewsets.ModelViewSet):
                 vehicle.save()
                 return Response({"message": "Car returned to station successfully"}, status=status.HTTP_200_OK)
             return Response({"error": "No active rental found"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": "You do not have permission to return a car to station"}, status=status.HTTP_403_FORBIDDEN)
+        return Response({"error": "You do not have permission to return a car to station"},
+                        status=status.HTTP_403_FORBIDDEN)
