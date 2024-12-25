a9e94d662d0107aad2afe42a361f7b9c134499a7
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-25 10:56:50 +0500

    feat(settings.py): SMS_RU_API_ID and MAX_DISTANCE variables created. MAX_DISTANCE for calculate user near station"

--- uzinfocom_rent_car_system_drf/settings.py
+++ uzinfocom_rent_car_system_drf/settings.py
@@ -201,3 +201,6 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+SMS_RU_API_ID = os.getenv('SMS_RU_API_ID')
+MAX_DISTANCE = float(os.getenv('MAX_DISTANCE'))
