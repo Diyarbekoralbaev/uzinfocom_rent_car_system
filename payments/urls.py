4e8b4289580b2d7ef2b0fcf5f516c6c0baf1492a
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-26 16:57:36 +0500

    refactor(payments/urls.py): url dublicate fix from /payments/payments/ to /payments/

--- payments/urls.py
+++ payments/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PaymentViewSet
 
 router = DefaultRouter()
-router.register(r'payments', PaymentViewSet)
+router.register(r'', PaymentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
