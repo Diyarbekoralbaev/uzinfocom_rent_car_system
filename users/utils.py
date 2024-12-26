a860f4247a6466c715cd80f6b5f91ea048db05be
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-26 15:06:25 +0500

    feat(users/utils.py): send_sms_otp created for send otp confirmation code to user via eskiz

--- users/utils.py
+++ users/utils.py
@@ -1,8 +1,14 @@
 from random import randint
 from django.core.mail import send_mail
 
-from uzinfocom_rent_car_system_drf.settings import DEFAULT_FROM_EMAIL
+from uzinfocom_rent_car_system_drf.settings import DEFAULT_FROM_EMAIL, ESKIZ_EMAIL, ESKIZ_PASSWORD
 
+from eskiz.client.sync import ClientSync
+
+eskiz_client= ClientSync(
+    email=ESKIZ_EMAIL,
+    password=ESKIZ_PASSWORD
+)
 
 def generate_otp():
     random = randint(1000, 9999)
@@ -18,3 +24,8 @@ def send_otp_email(email, otp):
         fail_silently=False,
     )
 
+def send_sms_otp(phone, otp):
+    eskiz_client.send_sms(
+        phone_number=phone,
+        message=f'Your OTP is: {otp}'
+    )
