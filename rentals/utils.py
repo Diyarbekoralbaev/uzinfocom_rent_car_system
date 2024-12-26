8b6df9020705ac7fc99f70861478a4f3104adb58
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-26 18:06:08 +0500

    refactor(rentals/utils.py): docstrings improved. send_email_notification function refactored for use celery task

--- rentals/utils.py
+++ rentals/utils.py
@@ -2,6 +2,7 @@ from math import radians, sin, cos, sqrt, atan2
 
 from django.conf import settings
 from django.core.mail import send_mail
+from .tasks import send_email_notifications_task
 
 
 def calculate_distance(lat1, lon1, lat2, lon2):
@@ -28,24 +29,22 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 def is_near_station(user_lat, user_lon, station_lat, station_lon, max_distance=settings.MAX_DISTANCE):
     """
     Determine if the user is within `max_distance` kilometers of the station.
+    :param user_lat: User's latitude
+    :param user_lon: User's longitude
+    :param station_lat: Station's latitude
+    :param station_lon: Station's longitude
+    :param max_distance: Maximum distance in kilometers
+    :return: True if the user is near the station, False otherwise
     """
     distance = calculate_distance(user_lat, user_lon, station_lat, station_lon)
     return distance <= max_distance
 
 
-def send_email(subject, to_email, message):
+def send_email_notification(user_id, subject, message):
     """
-    Send an email to the specified recipient.
+    Send email notifications to the user.
+    :param user_id: User ID
+    :param subject: Email subject
+    :param message: Email message
     """
-    try:
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[to_email],
-            fail_silently=False,
-        )
-    except Exception as e:
-        # Log the error or handle accordingly
-        # For simplicity, we'll pass here
-        pass
+    send_email_notifications_task.delay(user_id, subject, message)
