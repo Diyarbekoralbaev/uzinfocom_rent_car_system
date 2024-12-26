a560b842012a40492454a9cce99e1312f680f416
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-26 16:58:58 +0500

    feat(common/permissions.py): IsClient permission created

--- common/permissions.py
+++ common/permissions.py
@@ -8,6 +8,19 @@ class IsManager(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == UserChoice.MANAGER
 
+class IsClient(BasePermission):
+    """
+    Allows access only to users with the CLIENT role.
+    """
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        if request.user.role == UserChoice.CLIENT:
+            return True
+        if request.user.role == UserChoice.MANAGER and request.method in SAFE_METHODS:
+            return True
+        return False
+
 class IsOwnerOrManager(BasePermission):
     """
     Allows access to owners of the object or managers.
