e3a8ae2d7e220e69e5b57da13815b2201442a2d9
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-26 15:57:18 +0500

    feat(users/utils.py): functions for send confirmation message like notification to user's email

--- users/utils.py
+++ users/utils.py
@@ -1,31 +1,67 @@
 from random import randint
-from django.core.mail import send_mail
-
-from uzinfocom_rent_car_system_drf.settings import DEFAULT_FROM_EMAIL, ESKIZ_EMAIL, ESKIZ_PASSWORD
 
 from eskiz.client.sync import ClientSync
+from .models import UserModel
+from uzinfocom_rent_car_system_drf.settings import ESKIZ_EMAIL, ESKIZ_PASSWORD
+from .tasks import send_otp_email_task, send_sms_otp_task, send_email_notifications_task
 
-eskiz_client= ClientSync(
+eskiz_client = ClientSync(
     email=ESKIZ_EMAIL,
     password=ESKIZ_PASSWORD
 )
 
+
 def generate_otp():
+    """
+    Generate a random 4-digit OTP
+    :return: 4-digit OTP
+    """
     random = randint(1000, 9999)
     return random
 
 
 def send_otp_email(email, otp):
-    send_mail(
-        subject='Welcome to Rent Car System',
-        message=f'Your OTP is: {otp}',
-        from_email=DEFAULT_FROM_EMAIL,
-        recipient_list=[email],
-        fail_silently=False,
-    )
+    """
+    Send an OTP to the user's email
+    :param email: User's email
+    :param otp: OTP
+    """
+    send_otp_email_task.delay(email, otp)
+
 
 def send_sms_otp(phone, otp):
-    eskiz_client.send_sms(
-        phone_number=phone,
-        message=f'Your OTP is: {otp}'
-    )
+    """
+    Send an OTP to the user's phone number
+    :param phone: User's phone number
+    :param otp: OTP
+    """
+    send_sms_otp_task.delay(phone, otp)
+
+
+def send_registration_confirmation_email(user_id):
+    """
+    Send an email confirmation to the user after registration
+    :param user_id: User ID
+    """
+    subject = 'Registration Confirmation'
+    message = 'You have successfully registered.'
+    send_email_notifications_task.delay(user_id, subject, message)
+
+def send_password_change_notification(user_id):
+    """
+    Send an email notification to the user after changing the password
+    :param user_id: User ID
+    """
+    subject = 'Password Changed'
+    message = 'Your password has been changed successfully.'
+    send_email_notifications_task.delay(user_id, subject, message)
+
+
+def send_password_reset_notification(user_id):
+    """
+    Send an email notification to the user after resetting the password
+    :param user_id: User ID
+    """
+    subject = 'Password Reset'
+    message = 'Your password has been reset successfully.'
+    send_email_notifications_task.delay(user_id, subject, message)
