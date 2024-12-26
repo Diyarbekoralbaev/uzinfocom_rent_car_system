79c89cde02faa54f780f9971a9ad12b780b332e5
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2024-12-26 11:11:47 +0500

    feat(users/urls.py): UserView added to urls

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyView, LoginView, ChangePasswordView, ResetPasswordView, ResetPasswordConfirmView
+from .views import RegisterView, VerifyView, LoginView, ChangePasswordView, ResetPasswordView, ResetPasswordConfirmView, UserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
     path('reset-password-confirm/', ResetPasswordConfirmView.as_view(), name='reset-password-confirm'),
+    path('me/', UserView.as_view(), name='me'),
 ]
